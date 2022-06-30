d2f0c84f45d6f5e2f2708189d9d62c2b4d2577c6
남하욱 <2018008268@hanyang.ac.kr>
2022-06-30 17:43:50 +0900

    tap1 add click event

--- molcamp/app/src/main/java/com/examplemolcamp/MainActivity.kt
+++ molcamp/app/src/main/java/com/examplemolcamp/MainActivity.kt
@@ -1,34 +1,30 @@
 package com.examplemolcamp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.util.Log
+import android.widget.Adapter
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.test_layout.*
 import org.json.JSONArray
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    var phoneArray = arrayListOf<phone>(
-        phone("010-9954-6310","남하욱"),
-        phone("010-1234-1234","몰루")
-    )
+    var phoneArray = arrayListOf<phone>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*
+
+
         // Json 파일 열어서 String 으로 얻음
         val jsonString = assets.open("phoneNumbers.json").reader().readText()
-        Log.d("jsonString",jsonString)
 
         // jsonArray 로 파싱
         val jsonArray =JSONArray(jsonString)
-        Log.d("jsonArray", jsonArray.toString())
-
-        // string 배열
-
-        var nameArray = arrayOf("")
 
         // jsonArray 순회 : 인덱스별 JsonObject 취득후, key 에 해당하는 value 확인
         for (index in 0 until jsonArray.length()){
@@ -38,18 +34,23 @@ class MainActivity : AppCompatActivity() {
             val number = jsonObject.getString("number")
             val name = jsonObject.getString("name")
 
-            // string 배열에 각 값을 추가
-
-            numberArray = numberArray.plus(number)
-            nameArray = nameArray.plus(name)
-
+            // 배열에 각 값을 추가
+            phoneArray.add(phone(number,name))
         }
-        Log.d("numberArray", numberArray[1])
-*/
+
         setContentView(R.layout.activity_main)
+
+//        listview1.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+//            val selectItem = parent.getItemAtPosition(position) as phone
+//
+//            val intent = Intent(this, detailActivity::class.java)
+//            intent.putExtra("number",selectItem.number)
+//            intent.putExtra("name",selectItem.name)
+//            startActivity(intent)
+//            finish()
+//        }
+
         val phoneAdapter = MainListAdapter(this, phoneArray)
         listview1.adapter = phoneAdapter
-
-
     }
 }
