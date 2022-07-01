fa016f98d39662d8406533d7b8bb35d8999c9942
lee <stik@email.com>
2022-07-01 13:44:57 +0900

    220701_13:44

--- MA/app/src/main/java/com/example/myapplication3/detailActivity.kt
+++ MA/app/src/main/java/com/example/myapplication3/detailActivity.kt
@@ -1,8 +1,8 @@
-package com.examplemolcamp
+package com.example.myapplication3
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.detail_phone.*
 
 
--- molcamp/app/src/main/java/com/examplemolcamp/phone.kt
+++
@@ -1,3 +0,0 @@
-package com.examplemolcamp
-
-class phone (val number: String, val name: String)
